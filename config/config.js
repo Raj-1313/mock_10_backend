1f353fac74b49be00188c5b141a1f644f433132a
RAJ RATHOR <theromeo.1313@gmail.com>
2023-02-23 10:29:58 +0530

    added

--- config/config.js
+++
@@ -1,7 +0,0 @@
-const mongoose = require("mongoose");
-require("dotenv").config();
-
-mongoose.set("strictQuery", true);
-let connect = mongoose.connect(process.env.db_URL);
-
-module.exports = connect;
